d30fd5f4e0eeffbe00e6a0ecac429df92f4801e0
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-06-04 12:26:43 -0700

    tweaked so it builds slides from the Rmds now...

--- _build.sh
+++ _build.sh
@@ -2,5 +2,5 @@
 
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd','bookdown::gitbook')"
 
--- _slides_build.sh
+++ _slides_build.sh
@@ -2,7 +2,28 @@
 
 set -ev
 
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+#Rscript -e "bookdown::render_book('index.Rmd','bookdown::gitbook')"
 
-Rscript -e "bookdown::render_book('index.Rmd','xaringan::moon_reader',output_dir='docs/slides/', output_file='docs/slides/index.html')"
+# render slides day 3
+echo "book_filename: 'dllr-day3.Rmd'" > _tmp.yml
+cat _slides_base.yml >> _tmp.yml
+ls 3*Rmd | sed 's/^/  - "/' | sed 's/$/"/' >> _tmp.yml
+Rscript -e "bookdown::preview_chapter(list.files(pattern = '3.*Rmd'), 'rmarkdown::slidy_presentation',config_file='_tmp.yml')"
+mv dllr-day3.html docs/slides
+rm _tmp.yml
 
+# render slides day 4
+echo "book_filename: 'dllr-day4.Rmd'" > _tmp.yml
+cat _slides_base.yml >> _tmp.yml
+ls 4*Rmd | sed 's/^/  - "/' | sed 's/$/"/' >> _tmp.yml
+Rscript -e "bookdown::preview_chapter(list.files(pattern = '4.*Rmd'), 'rmarkdown::slidy_presentation',config_file='_tmp.yml')"
+mv dllr-day4.html docs/slides
+rm _tmp.yml
+
+# render slides day 5
+echo "book_filename: 'dllr-day5.Rmd'" > _tmp.yml
+cat _slides_base.yml >> _tmp.yml
+ls 5*Rmd | sed 's/^/  - "/' | sed 's/$/"/' >> _tmp.yml
+Rscript -e "bookdown::preview_chapter(list.files(pattern = '5.*Rmd'), 'rmarkdown::slidy_presentation',config_file='_tmp.yml')"
+mv dllr-day5.html docs/slides
+rm _tmp.yml
